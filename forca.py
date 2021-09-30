156efd68784f2bf6316b89accd28038f4c0a4622
Nicolas Magno <nicolasmagno.100@gmail.com>
2021-09-30 09:18:24 -0300

    modulo-3

--- forca.py
+++ forca.py
@@ -3,5 +3,21 @@ def jogar():
     print("**Seja bem-vindo ao jogo da Forca!**")
     print("*===================================*")
 
+    secret_word = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+        index = 0
+        
+        for letra in secret_word:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {}, na posição {}".format(letra, index))
+            index = index + 1
+        print("Jogando...")
+
 if(__name__ == "__main__"):
     jogar()
