a0c6470a93ba612054c49d90241c4ac616c55296
Nicolas Magno <nicolasmagno.100@gmail.com>
2021-09-30 23:05:14 -0300

    modulo-4

--- forca.py
+++ forca.py
@@ -4,20 +4,27 @@ def jogar():
     print("*===================================*")
 
     secret_word = "banana"
+    letras_corretas = ["_","_","_","_","_","_"]
 
     enforcou = False
     acertou = False
 
     while(not enforcou and not acertou):
+        print(letras_corretas)
         chute = input("Qual letra? ")
         chute = chute.strip()
         index = 0
         
         for letra in secret_word:
             if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {}, na posição {}".format(letra, index))
+                letras_corretas[index] = letra
+                letras_faltando = letras_corretas.count("_")
             index = index + 1
-        print("Jogando...")
+        if(letras_faltando == 0):
+            break
+        else:
+            print(letras_faltando)
+            print("Jogando...")
 
 if(__name__ == "__main__"):
     jogar()
