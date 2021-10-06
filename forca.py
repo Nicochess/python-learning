f47a40e728004608f53aa7086565861c3166cb0a
Nicolas Magno <nicolasmagno.100@gmail.com>
2021-10-06 01:03:52 -0300

    modulo-6

--- forca.py
+++ forca.py
@@ -3,24 +3,28 @@ def jogar():
     print("**Seja bem-vindo ao jogo da Forca!**")
     print("*===================================*")
 
-    secret_word = "banana"
-    letras_corretas = ["_","_","_","_","_","_"]
+    secret_word = "banana".upper()
+    letras_corretas = [ "_" for letra in secret_word]
 
     enforcou = False
     acertou = False
 
     while(not enforcou and not acertou):
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
         index = 0
+        erros = 0
         
         for letra in secret_word:
-            if(chute.upper() == letra.upper()):
+            if(chute == letra):
                 letras_corretas[index] = letra
                 letras_faltando = letras_corretas.count("_")
                 if(letras_faltando == 0):
                     print("Taraaaannnn, você ganhou!!!")
                     acertou = True
+            else:
+                erros += 1
+            
             index = index + 1
 
         print("{} letras faltando.".format(letras_faltando))
