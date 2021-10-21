34c6d82c6c0ee4e9f38e1f4a36be043d28399c02
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2021-10-20 23:43:56 -0300

    feat: regex url

--- url_extractor.py
+++ url_extractor.py
@@ -1,4 +1,6 @@
-url = "https://www.byte bank.com/cambio?moed aOrigem=real&moedaDesti no=dolar"
+import re
+
+url = "https://www.byte bank.com.br/cambio?moed aOrigem=real&moedaDesti no=dolar"
 
 #Sanitização dos dados
 url = url.replace(" ", "")
@@ -6,28 +8,25 @@ print(url)
 
 #O metodo find retorna o index da posição do caractere passado como parametro
 
-# search_query = url[interrogacao_index + 1:]
-
-search_param = 'moedaOrigem'
-
-param_index = url.find(search_param)
-
-e_comercial_index = url.find('&', param_index)
-
-equal_index = param_index + len(search_param)
-
-if (e_comercial_index == -1):
-    print(url[param_index:])
-else:
-    print(url[param_index:e_comercial_index])
-
-
 class Extractor_URL:
     def __init__(self, url):
         self.url = self.__sanitaze_url(url)
+        self.validate_url()
     
     def __sanitaze_url(self, url):
         return url.strip()
+    
+    def validate_url(self):
+        if not self.url:
+            raise ValueError('URL não pode estar vazia')
+        
+        url_pattern = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = url_pattern.match(self.url)
+        
+        if not match:
+            raise ValueError('URL é invalida.')
+        else:
+            print(match)
 
     @property
     def domain_site(self):
